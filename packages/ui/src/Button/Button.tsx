49d7f4800e2d13064e50797b74039be70f08a78f
Felipe <fcamparasilva@gmail.com>
2020-11-07 14:44:22 -0300

    fix(button): use clsx to merge classes

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import clsx from 'clsx'
 import { ButtonProps } from './types'
 import { Button } from './styles'
 
 export const ComponentButton: React.FC<ButtonProps> = ({
   label,
-  variant
+  type = 'button',
+  className,
+  variant,
+  ...props
 }) => {
-  const variantClassName = variant ? `button--${variant}` : ''
   return (
     <Button
-      className={variantClassName}
-      type="button"
+      {...props}
+      className={clsx(className, {
+        'button--contained': variant === 'contained',
+        'button--outline': variant === 'outline',
+        'button--secondary': variant === 'secondary'
+      })}
+      type={type}
     >
       {label}
     </Button>
