ab5d5ebff3d461d2f3f010cfece77505a18f8de6
Felipe <fcamparasilva@gmail.com>
2020-11-08 13:12:39 -0300

    feat(frontend): create layout header

--- packages/frontend/src/layouts/Default/index.tsx
+++ packages/frontend/src/layouts/Default/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { Button } from '@mlabs/ui'
+import UserSVG from 'src/assets/user.svg'
+import LogoSVG from 'src/assets/logo.svg'
+import BarSVG from 'src/assets/bar.svg'
+import 'src/layouts/Default/styles.css'
 
 const LayoutDefault: React.FC = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <>
+      <nav className="header header__container">
+        <img className="header__logo" src={LogoSVG} />
+        <Button
+          className="header__menu"
+          variant="contained"
+        >
+          <img src={BarSVG} />
+        </Button>
+        <div className="header__user">
+          <img src={UserSVG} />
+          <span className="header__user--name">
+            Anselmo Carlos
+          </span>
+        </div>
+      </nav>
+      <main className="main">{children}</main>
+    </>
+  )
 }
 
 export default LayoutDefault
--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PageHome: React.FC = () => {
-  return <div>teste</div>
+  return <div>Home</div>
 }
 
 export default PageHome
